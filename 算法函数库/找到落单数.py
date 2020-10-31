81f6c40c19aa5721f5bcbd42b05d2e8ab5d8565f
Freemanfirst <838616034@qq.com>
2020-10-31 23:50:43 +0800

    文件整理10.31

--- 算法函数库/找到落单数.py
+++
@@ -1,5 +0,0 @@
-def singleNumber(nums):
-    a = 0
-    for i in nums:
-        a ^= i
-    return a
--- 算法函数库/最大公约数gcd.py
+++
@@ -1,8 +0,0 @@
-# greatest common divisor
-# 欧几里得算法
-def gcd(a,b):
-    if a<b:
-        a,b=b,a
-    while b!=0:
-        a,b=b,a%b
-    return a
